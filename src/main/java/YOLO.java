bc4833a10b99e80594a1ec59de5dfdd6611f70a9
Sergii <arthasfp@mail.ru>
2016-06-12 13:05:03 +0300

    replaced fields

--- src/main/java/YOLO.java
+++ src/main/java/YOLO.java
@@ -46,8 +46,14 @@ public class YOLO {
         SSLContext.setDefault(ctx);
 
         long timestamp = new Date().getTime();
-        String hash = getMD5(getMD5(new User("apimaster", "apimaster").getPassword()) + String.valueOf(timestamp));
-        URL url = new URL("https://n29.epom.com/rest-api/sites.do?hash=" + hash + "&timestamp=" + timestamp + "&username=apimaster");
+        String hash = getMD5(getMD5(new User("apimaster","apimaster").getPassword()) + String.valueOf(timestamp));
+        int [] publishingCategories = null;
+        URL url = null;
+        if(publishingCategories == null){
+         url = new URL("https://n29.epom.com/rest-api/sites.do?hash=" + hash + "&timestamp=" + timestamp + "&username=apimaster");
+        }else {
+        url = new URL("https://n29.epom.com/rest-api/sites.do?hash=" + hash + "&timestamp=" + timestamp + "&username=apimaster&publishingCategories=" + publishingCategories);
+        }
         HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
         conn.setHostnameVerifier(new HostnameVerifier() {
             @Override
@@ -68,6 +74,26 @@ public class YOLO {
         }
         conn.disconnect();
         inputStream.close();
+
+        URL url2 = new URL("https://n29.epom.com/rest-api/zones/update.do?hash=" + hash + "&timestamp=" + timestamp + "&username=apimaster&name=fffff&description=jkdhjghn&siteId=2078");
+        HttpsURLConnection conn2 = (HttpsURLConnection) url2.openConnection();
+        conn2.setRequestMethod("POST");
+        conn2.setHostnameVerifier(new HostnameVerifier() {
+            @Override
+            public boolean verify(String arg0, SSLSession arg1) {
+                return true;
+            }
+        });
+        System.out.println(conn2.getResponseCode());
+        InputStream inputStream2 = conn2.getInputStream();
+        String myString2 = IOUtils.toString(inputStream2, "UTF-8");
+        myString2 = myString2.replace("{", "");
+        myString2 = myString2.replace("}", "");
+        System.out.println(myString2);
+        conn2.disconnect();
+        inputStream2.close();
+
+
     }
 
 
