8256e3c7a20b0905e6147e64b8fb00746b3fcae3
arthasfp <arthasfp@mail.ru>
2016-10-03 16:27:57 +0300

    Invocation Code enum was added

---
+++ src/main/java/InvocationCode.java
@@ -0,0 +1,16 @@
+public enum InvocationCode {
+    IFRAME_SYNC,
+    IFRAME_ASYNC,
+    IFRAME_SIMPLE,
+    JS_SYNC,
+    JS_ASYNC,
+    JW_PLAYER,
+    FLOW_PLAYER,
+    JSP,
+    PHP,
+    API,
+    DIRECT_URL,
+    EMAIL,
+    JW_PLAYER_OVERLAY,
+    API_JS_SYNC_CODE
+}
