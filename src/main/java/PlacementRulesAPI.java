1fdca87d98b84bb3bd8e32b5a5fea6d6f6869da1
arthasfp <arthasfp@mail.ru>
2016-10-12 15:40:58 +0300

    PlacementRulesAPI class was added

---
+++ src/main/java/PlacementRulesAPI.java
@@ -0,0 +1,58 @@
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDate;
+import java.util.Date;
+
+public class PlacementRulesAPI {
+    private User user;
+
+    public PlacementRulesAPI(User user) {
+        this.user = user;
+    }
+
+
+
+
+    private void getVerifierForConnection(HttpsURLConnection conn) {
+        conn.setHostnameVerifier(new HostnameVerifier() {
+            @Override
+            public boolean verify(String arg0, SSLSession arg1) {
+                return true;
+            }
+        });
+    }
+
+    public long getTimestamp() {
+        return new Date().getTime();
+    }
+
+    public String getHash() throws NoSuchAlgorithmException {
+        String hash = getMD5(getMD5(user.getPassword()) + String.valueOf(getTimestamp()));
+        return hash;
+    }
+
+    private String getMD5(String data) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(data.getBytes());
+        byte byteData[] = md.digest();
+        StringBuilder sb = new StringBuilder();
+        for (byte aByteData : byteData) {
+            sb.append(Integer.toString((aByteData & 0xff) + 0x100, 16).substring(1));
+        }
+        return sb.toString();
+    }
+
+    private LocalDate getDateFromString(String value){
+        String[] newValue = value.split("-");
+        for (String a: newValue){
+            System.out.println(a);
+        }
+        if (newValue.length > 3){
+            System.out.println("Wrong input!!!");
+        }
+        return LocalDate.of(Integer.parseInt(newValue[0]), Integer.parseInt(newValue[1]), Integer.parseInt(newValue[2]));
+    }
+}
