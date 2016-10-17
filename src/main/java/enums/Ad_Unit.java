44f6b26c8170aec2765effd8cd32b26670d545ab
arthasfp <arthasfp@mail.ru>
2016-10-17 18:29:47 +0300

    Ad_Unit enum

---
+++ src/main/java/enums/Ad_Unit.java
@@ -0,0 +1,15 @@
+package enums;
+
+public enum Ad_Unit {
+    MEDIUM_RECTANGLE,
+    RECTANGLE,
+    LEADERBOARD,
+    FULL_BANNER,
+    SKYSCRAPER,
+    WIDE_SKYSCRAPER,
+    HALF_PAGE_AD,
+    BUTTON_2,
+    MICRO_BAR,
+    XXL_BOX,
+    CUSTOM_SITE_BANNER_SIZE
+}
